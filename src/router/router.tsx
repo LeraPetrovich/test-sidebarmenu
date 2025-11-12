4954d8e70aca891995c852dc934a05c8e56b2872
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-11-12 19:54:18 +0300

    fix routing

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -17,31 +17,26 @@ import {
 
 //конфигурация доступных страниц
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          path: "",
-          element: <MainPage />,
-        },
-        { path: "trends", element: <TrendsPage /> },
-        { path: "tasks", element: <TasksPage /> },
-        { path: "tickets", element: <TicketsPage /> },
-        { path: "products", element: <ProductsPage /> },
-        { path: "orders", element: <OrdersPage /> },
-        { path: "supplies", element: <SuppliesPage /> },
-        { path: "list", element: <ListPage /> },
-        { path: "reviews", element: <ReviewsPage /> },
-        { path: "notification", element: <NotificationPage /> },
-        { path: "shop", element: <ShopPage /> },
-        { path: "*", element: <NotFoundPage /> },
-      ],
-    },
-  ],
+export const router = createBrowserRouter([
   {
-    basename: "/test-sidebarmenu", // это для github pages
-  }
-);
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <MainPage />,
+      },
+      { path: "trends", element: <TrendsPage /> },
+      { path: "tasks", element: <TasksPage /> },
+      { path: "tickets", element: <TicketsPage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "orders", element: <OrdersPage /> },
+      { path: "supplies", element: <SuppliesPage /> },
+      { path: "list", element: <ListPage /> },
+      { path: "reviews", element: <ReviewsPage /> },
+      { path: "notification", element: <NotificationPage /> },
+      { path: "shop", element: <ShopPage /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
+]);
