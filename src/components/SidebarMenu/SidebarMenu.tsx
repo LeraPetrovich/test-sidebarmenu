20abdc204cbb5b04e5ab2a09a6125b4d364c028f
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-11-12 14:45:11 +0300

    create open close menu

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,6 +1,7 @@
 import { useState, useMemo } from "react";
 import { mainLayoutConfig } from "../../configs";
 import { SidebarMenuItem } from "../SidebarMenuItem";
+import { DynamicIcon } from "lucide-react/dynamic";
 
 export const SidebarMenu = () => {
   const [open, setOpen] = useState(true);
@@ -13,9 +14,25 @@ export const SidebarMenu = () => {
       ].join(" ")}
     >
       <div className="flex flex-col px-4 gap-2 py-5">
-        {mainLayoutConfig.map((item) => {
-          return <SidebarMenuItem data={item} />;
-        })}
+        <div className="flex flex-col gap-2">
+          {mainLayoutConfig.map((item) => {
+            return (
+              <SidebarMenuItem key={item.path} isOpen={open} data={item} />
+            );
+          })}
+        </div>
+        <div className="border-t-1 border-slate-200 pt-2">
+          <button
+            onClick={() => setOpen((open) => !open)}
+            className="flex items-center gap-2 p-[5px] rounded-lg transition-colors w-max text-left hover:bg-zinc-200 text-black"
+          >
+            <DynamicIcon
+              name={open ? "arrow-left-from-line" : "arrow-right-from-line"}
+              size={20}
+              color="black"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/SidebarMenuItem/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem/SidebarMenuItem.tsx
@@ -1,9 +1,12 @@
-import { type FC, useState, useMemo, useEffect } from "react";
+import { type FC, useState, useMemo, useEffect, memo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import type { MenuItemType } from "../../configs/types";
 import { DynamicIcon } from "lucide-react/dynamic";
 
-export const SidebarMenuItem: FC<{ data: MenuItemType }> = ({ data }) => {
+const MemoSidebarMenuItem: FC<{ data: MenuItemType; isOpen: boolean }> = ({
+  data,
+  isOpen,
+}) => {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
   const location = useLocation();
 
@@ -15,36 +18,47 @@ export const SidebarMenuItem: FC<{ data: MenuItemType }> = ({ data }) => {
   }, [location.pathname]);
 
   useEffect(() => {
-    if (!isActiveParent && isOpenDropdown) {
+    if (!isActiveParent && isOpenDropdown && isOpen) {
       setIsOpenDropdown(false);
     }
-  }, [isActiveParent]);
+  }, [isActiveParent, isOpen]);
 
   if (data.children?.length) {
     return (
       <div className="flex flex-col">
         <button
-          onClick={() => setIsOpenDropdown((open) => !open)}
+          onClick={() => {
+            if (isOpen) setIsOpenDropdown((open) => !open);
+          }}
           className={[
-            "cursor-pointer flex items-center gap-2 px-4 py-2 rounded-lg transition-colors w-full text-left",
+            "cursor-pointer flex items-center rounded-lg transition-colors w-full text-left",
             isActiveParent
               ? "text-sky-700 [&_svg]:stroke-sky-700 hover:bg-zinc-200"
               : "hover:bg-zinc-200 text-black",
+            data.icon !== null && data.icon !== undefined
+              ? "pr-4"
+              : "px-3 py-1",
           ].join(" ")}
         >
-          {data.icon && <DynamicIcon name={data.icon as any} size={20} />}
-          <span className="flex-1">{data.title}</span>
-          <DynamicIcon
-            name={isOpenDropdown ? "chevron-up" : ("chevron-down" as any)}
-            size={16}
-            color="black"
-          />
+          {data.icon && (
+            <div className="p-[5px] rounded-lg transition-colors w-max text-left">
+              <DynamicIcon name={data.icon as any} size={20} color="black" />
+            </div>
+          )}
+          {isOpen && <span className="flex-1">{data.title}</span>}
+          {isOpen && (
+            <DynamicIcon
+              name={isOpenDropdown ? "chevron-up" : ("chevron-down" as any)}
+              size={16}
+              color="black"
+            />
+          )}
         </button>
 
-        {isOpenDropdown && (
+        {isOpenDropdown && isOpen && (
           <div className="ml-6 mt-1 flex flex-col gap-1">
             {data.children.map((child) => (
-              <SidebarMenuItem key={child.path} data={child} />
+              <SidebarMenuItem isOpen={isOpen} key={child.path} data={child} />
             ))}
           </div>
         )}
@@ -56,14 +70,21 @@ export const SidebarMenuItem: FC<{ data: MenuItemType }> = ({ data }) => {
     <NavLink
       to={data.path}
       className={[
-        "flex items-center gap-2 px-4 py-2 rounded-lg transition-colors w-full text-left",
+        "flex items-center  rounded-lg transition-colors w-full text-left",
         isActiveParent
           ? "bg-sky-100 text-sky-700 [&_svg]:stroke-sky-700"
           : "hover:bg-zinc-200 text-black",
+        data.icon !== null && data.icon !== undefined ? "pr-4" : "px-3 py-1",
       ].join(" ")}
     >
-      {data.icon && <DynamicIcon name={data.icon as any} size={20} />}
-      <span>{data.title}</span>
+      {data.icon && (
+        <div className="p-[5px] rounded-lg transition-colors w-max text-left">
+          <DynamicIcon name={data.icon as any} size={20} color="black" />
+        </div>
+      )}
+      {isOpen && <span>{data.title}</span>}
     </NavLink>
   );
 };
+
+export const SidebarMenuItem = memo(MemoSidebarMenuItem);
