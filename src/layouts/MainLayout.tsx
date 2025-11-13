79bbc28cded7076195012e8a966c00c3f44b1507
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-11-13 16:51:42 +0300

    update position help panel menu

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,17 +2,20 @@ import { type FC } from "react";
 import { Outlet } from "react-router-dom";
 import { HelpMenu } from "../components";
 import { SidebarMenu } from "../components";
+import { HelpPanelContextProvider } from "../contexts/HelpPanelContext";
 
 //удобный layout чтобы отрисовывать сменные страницы и не дублировать меню sidebar
 
 export const MainLayout: FC = () => {
   return (
-    <div className="flex w-full h-full sm:flex-row flex-col">
-      <SidebarMenu />
-      <div className="flex-1 bg-white transition-all duration-300 flex flex-col sm:order-2 order-1">
-        <Outlet />
+    <HelpPanelContextProvider>
+      <div className="flex w-full h-full sm:flex-row flex-col">
+        <SidebarMenu />
+        <div className="flex-1 bg-white transition-all duration-300 flex flex-col sm:order-2 order-1">
+          <Outlet />
+        </div>
+        <HelpMenu />
       </div>
-      <HelpMenu />
-    </div>
+    </HelpPanelContextProvider>
   );
 };
